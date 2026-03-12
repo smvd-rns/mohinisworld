67d24c7470aba108ef7680054b5bf1367f689861
SMVD <smvd@voicepune.com>
2026-03-12 19:30:54 +0530

    middle way

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/request';
+import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   // Cloudflare always adds the 'cf-ray' header.
